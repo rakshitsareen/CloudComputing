bd672248ddefb6a741ce60c11a0b3877c587b02a
Dhruv Arora <dhruv@10-18-159-175.dynapool.wireless.nyu.edu>
2018-10-16 22:33:44 -0400

    Added Chatbot Lambda

---
+++ ChatBot/Chatbot Lambda/chatbot/src/main/java/com/cloud/chatbotEntity/ChatBotRequest.java
@@ -0,0 +1,28 @@
+package com.cloud.chatbotEntity;
+
+public class ChatBotRequest {
+	
+	public String message;
+	public String username;
+	
+	
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	} 
+	
+	
+
+}
